7ecf939c33bc00bd649bba31f6c30205bb4c478b
Izzat-Zdin <izzatzdin375@gmail.com>
2025-02-26 11:16:15 +0800

    update

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
-<?php
-require __DIR__ . '/../index.php';
+{
+  "version": 2,
+  "builds": [
+    { "src": "api/index.php", "use": "@vercel/php" },
+    { "src": "api/(.*)", "use": "@vercel/php" }
+  ],
+  "routes": [
+    { "src": "/(.*)", "dest": "/api/index.php" }
+  ]
+}
