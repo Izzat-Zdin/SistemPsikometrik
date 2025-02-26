7dd83d0853c8c3e3526b0ce857260a238d416638
Izzat-Zdin <izzatzdin375@gmail.com>
2025-02-26 11:08:31 +0800

    edit

--- PJ-Guru/action/connect.php
+++ PJ-Guru/action/connect.php
@@ -1,22 +1,19 @@
 <?php	
 
-	$host = "localhost";
-	$dbusername = "root";
-	$dbpassword = "";
-	$dbname = "pj";
-	
-	
-	
-	//Database connection
-	$condb = mysqli_connect($host,$dbusername,$dbpassword,$dbname);
-	if(!$condb)
-	{
-		die("gagal");
-	}
-	else
-	{
-		#die("berjaya");
-	}
-	
-	
+$host = "smkpl.h.filess.io"; // Remote database host
+$dbusername = "pj_creaturehe"; // Your database username
+$dbpassword = "05b97a4fe0b4d15e14f861aa1399b8280237659a"; // Your database password
+$dbname = "pj_creaturehe"; // Your database name
+$port = 3307; // The port number
+
+// Database connection
+$condb = mysqli_connect($host, $dbusername, $dbpassword, $dbname, $port);
+
+// Check connection
+if (!$condb) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully!";
+}
+
 ?>
